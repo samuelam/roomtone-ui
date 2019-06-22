0f679ceac74dc0375d7e5f92244d9eaced26de0b
Sam Miller <samuel@roomtone.com>
2019-06-22 03:33:23 -0700

    Reformat JS files using prettier

--- spec/theme-spec.js
+++ spec/theme-spec.js
@@ -13,7 +13,6 @@ describe(`${themeName} theme`, () => {
   });
 
   it('allows the tab sizing to be set via config', () => {
-<<<<<<< HEAD
     atom.config.set(`${themeName}.tabSizing`, 'Maximum');
     expect(
       document.documentElement.getAttribute(`theme-${themeName}-tabsizing`)
@@ -29,28 +28,10 @@ describe(`${themeName} theme`, () => {
 
   it('allows the tab close button to be shown on the left via config', () => {
     atom.config.set(`${themeName}.tabCloseButton`, 'Left');
-=======
-    atom.config.set(`${themeName}.tabSizing`, 'Maximum')
-    expect(
-      document.documentElement.getAttribute(`theme-${themeName}-tabsizing`)
-    ).toBe('maximum')
-  })
-
-  it('allows the tab sizing to be set via config', () => {
-    atom.config.set(`${themeName}.tabSizing`, 'Minimum')
-    expect(
-      document.documentElement.getAttribute(`theme-${themeName}-tabsizing`)
-    ).toBe('minimum')
-  })
-
-  it('allows the tab close button to be shown on the left via config', () => {
-    atom.config.set(`${themeName}.tabCloseButton`, 'Left')
->>>>>>> 65ba78b385acd732b91668cea7a9bf79231bf3b8
     expect(
       document.documentElement.getAttribute(
         `theme-${themeName}-tab-close-button`
       )
-<<<<<<< HEAD
     ).toBe('left');
   });
 
@@ -75,29 +56,3 @@ describe(`${themeName} theme`, () => {
     ).toBe(null);
   });
 });
-=======
-    ).toBe('left')
-  })
-
-  it('allows the dock toggle buttons to be hidden via config', () => {
-    atom.config.set(`${themeName}.hideDockButtons`, true)
-    expect(
-      document.documentElement.getAttribute(`theme-${themeName}-dock-buttons`)
-    ).toBe('hidden')
-  })
-
-  it('allows the tree-view headers to be sticky via config', () => {
-    atom.config.set(`${themeName}.stickyHeaders`, true)
-    expect(
-      document.documentElement.getAttribute(`theme-${themeName}-sticky-headers`)
-    ).toBe('sticky')
-  })
-
-  it('allows the tree-view headers to not be sticky via config', () => {
-    atom.config.set(`${themeName}.stickyHeaders`, false)
-    expect(
-      document.documentElement.getAttribute(`theme-${themeName}-sticky-headers`)
-    ).toBe(null)
-  })
-})
->>>>>>> 65ba78b385acd732b91668cea7a9bf79231bf3b8
